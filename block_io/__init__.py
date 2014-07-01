bfd9191468987092ad5ae55d24083026c4aa66a4
Atif Nazir <atifn@atifn.net>
2014-07-01 10:58:39 -0700

    first commit

---
+++ block_io/__init__.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+VERSION="0.1"
+
+class BlockIo(object):
+
+    def __init__(self, options = {}):
+        # initiate the object
+        self.api_key = options['api_key']
+        self.base_url = 'https://block.io/api/v1/API_CALL/'
+
+    def __getattr__(self, attr, *args, **kwargs):
+        
+        def hook(*args, **kwargs):
+            return self.api_call(attr, **kwargs)
+        return hook
+
+    def api_call(self, method, **kwargs):
+        # the actual API call
+        
+        # http parameters
+        payload = {}
+
+        if self.api_key is not None:
+            payload["api_key"] = self.api_key
+            
+        payload.update(kwargs)
+
+        # update the parameters with the API key
+        response = requests.get(self.base_url.replace('API_CALL',method), params = payload)
+
+        return response.json()
+
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='block_io',
+      version='0.1',
+      description='The easiest way to create a Dogecoin, Bitcoin, or Litecoin wallet. Sign up at Block.io for your API key.',
+      url='http://github.com/blockio/blockio-python',
+      author='Atif Nazir',
+      author_email='a@block.io',
+      license='MIT',
+      packages=['block_io'],
+      install_requires=[
+          'requests',
+          'json',
+      ],
+      zip_safe=False)
+
+# for packages not on pypi
+# dependency_links=['http://github.com/user/repo/tarball/master#egg=package-1.0']
