8a917cdada948ad8b7192366941457c1c47ce387
Atif Nazir <atifn@atifn.net>
2018-12-05 01:48:10 +0000

    lock dependency versions + version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='block-io',
-      version='1.1.8',
+      version='1.1.9',
       description='The easiest way to integrate Bitcoin, Dogecoin and Litecoin in your applications. Sign up at Block.io for your API key.',
       url='https://github.com/BlockIo/block_io-python',
       author='Atif Nazir',
@@ -9,11 +9,11 @@ setup(name='block-io',
       license='MIT',
       packages=['block_io'],
       install_requires=[
-          'requests',
-          'pycryptodome',
-          'ecdsa',
-          'six',
-          'base58'
+          'requests>=2.20.0',
+          'pycryptodome==3.6.6',
+          'ecdsa==0.13',
+          'six>=1.8.0',
+          'base58>=1.0.0'
       ],
       zip_safe=False)
 
