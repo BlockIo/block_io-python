55f6ca955e70cdb1c6617bd26acec80ea9cbfeca
Atif Nazir <a@block.io>
2021-09-27 15:08:52 -0700

    update bitcoinutils to use bech32m + version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
     
 setup(name='block-io',
-      version='2.0.4',
+      version='2.0.5',
       description='The easiest way to integrate Bitcoin, Dogecoin and Litecoin in your applications. Sign up at Block.io for your API key.',
       url='https://github.com/BlockIo/block_io-python',
       author='Atif Nazir',
@@ -18,6 +18,6 @@ setup(name='block-io',
           'requests>=2.25.0',
           'pycryptodome>=3.9.9,<4.0',
           'base58==2.1.0',
-          'bitcoin-utils-fork-minimal==0.4.11.3'
+          'bitcoin-utils-fork-minimal==0.4.11.4'
       ],
       zip_safe=False)
